66e6a557a82778fcc0bdefb12e3694535c4c3e6a
Shaktijeet Sahoo <sahooshaktijeet@gmail.com>
2021-11-23 22:49:32 +0530

    reworked navbar and top section

--- js/main.js
+++ js/main.js
@@ -1,25 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-  
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
-  
-      // Add a click event on each of them
-      $navbarBurgers.forEach( el => {
-        el.addEventListener('click', () => {
-  
-          // Get the target from the "data-target" attribute
-          const target = el.dataset.target;
-          const $target = document.getElementById(target);
-  
-          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-          el.classList.toggle('is-active');
-          $target.classList.toggle('is-active');
-  
-        });
+  // hamburger
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach(el => {
+      let children = el.children;
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+      el.addEventListener('click', function(event){
+        console.log(event.currentTarget);
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
       });
-    }
-  
-  });
+      el.addEventListener('touchstart', () => {
+          // el.click();
+      });
+    });
+  }
+
+  // link buttons
+  const navlogo = document.querySelector('.navbar-item img');
+  navlogo.addEventListener('click', () => {
+      window.open(encodeURI('https://github.com/Jax-Core'), 'target=_blank')
+  })
+});
