a1f812829c7b0ee5f00800ca44a37ff66b71d808
Shaktijeet Sahoo <sahooshaktijeet@gmail.com>
2021-11-27 19:20:15 +0530

    added gitignore

--- js/main.js
+++ js/main.js
@@ -1,34 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // hamburger
-  // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(
-    document.querySelectorAll(".navbar-burger"),
-    0
-  );
+	// hamburger
+	// Get all "navbar-burger" elements
+	const $navbarBurgers = Array.prototype.slice.call(
+		document.querySelectorAll('.navbar-burger'),
+		0
+	)
 
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
-    // Add a click event on each of them
-    $navbarBurgers.forEach((el) => {
-      let children = el.children;
-      // Get the target from the "data-target" attribute
-      const target = el.dataset.target;
-      const $target = document.getElementById(target);
-      el.addEventListener("click", function (event) {
-        console.log(event.currentTarget);
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle("is-active");
-        $target.classList.toggle("is-active");
-      });
-      el.addEventListener("touchstart", () => {
-        // el.click();
-      });
-    });
-  }
+	// Check if there are any navbar burgers
+	if ($navbarBurgers.length > 0) {
+		// Add a click event on each of them
+		$navbarBurgers.forEach((el) => {
+			let children = el.children
+			// Get the target from the "data-target" attribute
+			const target = el.dataset.target
+			const $target = document.getElementById(target)
+			el.addEventListener('click', function (event) {
+				console.log(event.currentTarget)
+				// Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+				el.classList.toggle('is-active')
+				$target.classList.toggle('is-active')
+			})
+			el.addEventListener('touchstart', () => {
+				// el.click();
+			})
+		})
+	}
 
-  // link buttons
-  const navlogo = document.querySelector(".navbar-item img");
-  navlogo.addEventListener("click", () => {
-    window.open(encodeURI("https://jax-core.github.io"), "target=_blank");
-  });
+	// link buttons
+	const navlogo = document.querySelector('.navbar-item img')
+	navlogo.addEventListener('click', () => {
+		window.open(encodeURI('https://jax-core.github.io'), 'target=_blank')
+	})
 });
